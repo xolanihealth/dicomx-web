ae644e2a69eeb6afddb5ce3ca5df9f95684fe326
MACBOOK <macbook@MACBOOKs-MacBook-Pro.local>
2023-05-12 22:12:44 +0100

    implementing cornerstone

---
+++ src/container/pages/Viewer/datas.js
@@ -0,0 +1,29 @@
+export const tools = [
+  // Mouse
+  {
+    name: 'Wwwc',
+    mode: 'active',
+    modeOptions: { mouseButtonMask: 1 },
+  },
+  {
+    name: 'Zoom',
+    mode: 'active',
+    modeOptions: { mouseButtonMask: 2 },
+  },
+  {
+    name: 'Pan',
+    mode: 'active',
+    modeOptions: { mouseButtonMask: 4 },
+  },
+  // Scroll
+  { name: 'StackScrollMouseWheel', mode: 'active' },
+  // Touch
+  { name: 'PanMultiTouch', mode: 'active' },
+  { name: 'ZoomTouchPinch', mode: 'active' },
+  { name: 'StackScrollMultiTouch', mode: 'active' },
+];
+
+export const imageIds = [
+  'dicomweb://s3.amazonaws.com/lury/PTCTStudy/1.3.6.1.4.1.25403.52237031786.3872.20100510032220.11.dcm',
+  'dicomweb://s3.amazonaws.com/lury/PTCTStudy/1.3.6.1.4.1.25403.52237031786.3872.20100510032220.12.dcm',
+];
